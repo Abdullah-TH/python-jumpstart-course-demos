e8c9d704b9de24526943ae09b07ad0b011046bd9
Abdullah Althobetey <abdullah.th@me.com>
2020-04-08 23:09:09 +0300

    Add doc-strings to the journal module

--- apps/04_journal/you_try/journal.py
+++ apps/04_journal/you_try/journal.py
@@ -1,7 +1,21 @@
+"""
+The journal module consists of 4 functions
+load(name)
+save(name, journal_data)
+get_full_path_name(name)
+add_entry(text, journal_data)
+"""
 import os
 
 
 def load(name):
+    """
+    Load the journal data from a file and create a list of texts
+    that represents the journal
+
+    :param name: name of the journal file
+    :return: a list of text represents the journal
+    """
     filename = get_full_path_name(name)
     data = []
 
@@ -14,6 +28,12 @@ def load(name):
 
 
 def save(name, journal_data):
+    """
+    Save the journal_data to a file 'name'
+    :param name: name of the file to save the journal data
+    :param journal_data: the journal data as a list of text
+    :return: None
+    """
     filename = get_full_path_name(name)
     print(f'..... saving to {filename}')
 
@@ -23,9 +43,22 @@ def save(name, journal_data):
 
 
 def get_full_path_name(name):
+    """
+    Get the full path of the file 'name'
+
+    :param name: the name of the file
+    :return: full path of the file 'name'
+    """
     filename = os.path.abspath(os.path.join('.', 'journals', name + '.jrl'))
     return filename
 
 
 def add_entry(text, journal_data):
+    """
+    Add text entry to the journal_data
+
+    :param text: text to add to the journal
+    :param journal_data: the journal data as a list of text
+    :return: None
+    """
     journal_data.append(text)
